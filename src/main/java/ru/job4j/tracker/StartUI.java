118d407052f2d9a2f3d05ac045e2a7343f2c87fe
Илья <ily470@yandex.ru>
2022-11-25 18:45:34 +0700

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item1 = new Item("тест");
+        System.out.println(item1);
+    }
+}
