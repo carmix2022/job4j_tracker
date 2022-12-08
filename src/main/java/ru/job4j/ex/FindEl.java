b48c8091c30315a9db2e507dbbd6e9d36b1e117d
Илья <ily470@yandex.ru>
2022-12-09 00:16:42 +0700

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("fail!");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"раз", "два", "три"}, "дfbва"));
+        } catch (ElementNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.getStackTrace();
+        }
+    }
+}
