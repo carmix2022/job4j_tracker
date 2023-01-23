cace2e3f7a666d1c80dfc06c3b6e0d91cf359d8e
Илья <ily470@yandex.ru>
2023-01-23 14:09:26 +0700

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,14 +9,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                String rsl;
-                if ("".equals(start)) {
-                    rsl = start +  el;
-                } else {
-                    rsl = start + "/" +  el;
-                }
-                tmp.add(rsl);
-                start = rsl;
+                start = "".equals(start) ? start +  el : start + "/" +  el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
