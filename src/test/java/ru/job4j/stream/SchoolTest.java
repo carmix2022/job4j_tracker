c45e7472fc1b79ae7e3dc85b3dee74da7e3602e0
Илья <ily470@yandex.ru>
2023-02-04 03:19:44 +0700

    5. Фильтрация учеников [#110058]

---
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SchoolTest {
+    @Test
+    public void whenCollectClassA() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(40, "Surname4"),
+                new Student(50, "Surname5"),
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9")
+        );
+        School school = new School();
+        Predicate<Student> predicate1 = x -> x.getScore() >= 70;
+        Predicate<Student> predicate2 = x -> x.getScore() <= 100;
+        Predicate<Student> predicate = predicate1.and(predicate2);
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(70, "Surname7"));
+        expected.add(new Student(90, "Surname9"));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenCollectClassB() {
+        List<Student> students = List.of(
+                new Student(20, "Surname2"),
+                new Student(30, "Surname3"),
+                new Student(50, "Surname5"),
+                new Student(60, "Surname6"),
+                new Student(80, "Surname8")
+        );
+        School school = new School();
+        Predicate<Student> predicate1 = x -> x.getScore() >= 50;
+        Predicate<Student> predicate2 = x -> x.getScore() < 70;
+        Predicate<Student> predicate = predicate1.and(predicate2);
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(50, "Surname5"));
+        expected.add(new Student(60, "Surname6"));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenCollectClassV() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(40, "Surname4"),
+                new Student(60, "Surname6"),
+                new Student(90, "Surname9")
+        );
+        School school = new School();
+        Predicate<Student> predicate1 = x -> x.getScore() > 0;
+        Predicate<Student> predicate2 = x -> x.getScore() < 50;
+        Predicate<Student> predicate = predicate1.and(predicate2);
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = new ArrayList<>();
+        expected.add(new Student(10, "Surname1"));
+        expected.add(new Student(30, "Surname3"));
+        expected.add(new Student(40, "Surname4"));
+        assertThat(result).isEqualTo(expected);
+    }
+}
