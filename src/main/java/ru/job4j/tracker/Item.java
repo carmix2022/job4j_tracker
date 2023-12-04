20e02b98da99b2d73f2b3f3e405a474804b2d687
Илья <ily470@yandex.ru>
2023-12-04 17:06:23 +0700

    2. ManyToMany [#331986 #463895]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,43 +1,45 @@
 package ru.job4j.tracker;
 
 import lombok.*;
+import ru.job4j.toone.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Entity
 @Table(name = "items")
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private int id;
     @NonNull
-    @Getter
-    @Setter
     private String name;
-    @Getter
+
     @EqualsAndHashCode.Exclude
     private LocalDateTime created = LocalDateTime.now().withNano(0);
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "item_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
+    public Item(int id, String name, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
     }
 }
